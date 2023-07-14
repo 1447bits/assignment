df951fe97c120ffe624b5c693ee879668a799272
Atharv Vyas <95745167+1447bits@users.noreply.github.com>
2023-07-14 19:07:27 +0530

    call api

---
+++ index.js
@@ -0,0 +1,27 @@
+const main = document.getElementById("main")
+
+var disp_data = "";
+
+// play loading until fetch
+main.innerHTML = "Loading...";
+
+fetch("https://reqres.in/api/users?page=1")
+
+    .then(response => response.json())
+    .then(data => {
+        disp_data = JSON.stringify(data);
+
+        // load the main App
+        loadApp()
+    })
+    .catch(error => {
+        // Handle any errors that occur during the fetch
+        console.error('Error:', error);
+
+    });
+
+
+function loadApp() {
+    main.innerHTML = disp_data;
+}
+
